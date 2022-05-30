e3061dd044fa6af83b7eb5438fe4d34c7fc51ad7
HarshTripathi12 <95767789+HarshTripathi12@users.noreply.github.com>
2022-05-30 17:53:03 +0530

    Add files via upload

---
+++ Non local Variable.py
@@ -0,0 +1,17 @@
+def greet ():
+    def function1 ():
+        a=0
+    def function2 ():
+        a=1
+    def function3 ():
+        a=99
+    a=4
+    function1 ()
+    print (a)
+    function2 ()
+    print (a)
+    function3 ()
+    print (a)
+a=100
+greet ()
+print(a)
