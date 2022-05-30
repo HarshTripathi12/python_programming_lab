82be4999fbf1dc1d5aa858bac25c690104f4212a
HarshTripathi12 <95767789+HarshTripathi12@users.noreply.github.com>
2022-05-30 19:27:06 +0530

    Add files via upload

---
+++ scan the value.py
@@ -0,0 +1,7 @@
+num = int(input('Enter the number of item'))
+dct = {}
+for i in range (num):
+    key = input('Enter the number')
+    val = input('Enter the number')
+    dct[key] = val
+print(dct)
